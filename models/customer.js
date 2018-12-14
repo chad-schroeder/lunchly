6f6ba604b9503bebf9c4d0a2604bb62575d8ef7f
Jonathan <jkung92@gmail.com>
2018-12-14 15:15:24 -0800

    Part 10 Complete

--- models/customer.js
+++ models/customer.js
@@ -1,18 +1,23 @@
 /** Customer for Lunchly */
 
-const db = require("../db");
-const Reservation = require("./reservation");
-
+const db = require('../db');
+const Reservation = require('./reservation');
 
 /** Customer of the restaurant. */
 
 class Customer {
-  constructor({id, firstName, lastName, phone, notes}) {
+  constructor({ id, firstName, middleName, lastName, phone, notes }) {
     this.id = id;
     this.firstName = firstName;
+    this.middleName = middleName || '';
     this.lastName = lastName;
     this.phone = phone;
     this.notes = notes;
+  }
+
+  /** method for getting a customer's full name */
+  get fullName() {
+    return `${this.firstName} ${this.middleName} ${this.lastName}`;
   }
 
   /** methods for getting/setting notes (keep as empty string, not NULL) */
@@ -39,7 +44,7 @@ class Customer {
 
   static async all() {
     const results = await db.query(
-          `SELECT id, 
+      `SELECT id, 
          first_name AS "firstName",  
          last_name AS "lastName", 
          phone, 
@@ -50,17 +55,48 @@ class Customer {
     return results.rows.map(c => new Customer(c));
   }
 
+  /** get a customer by name. */
+  static async search(name) {
+    const results = await db.query(
+      `SELECT id, 
+      first_name AS "firstName",  
+      last_name AS "lastName", 
+      phone, 
+      notes 
+     FROM customers
+     WHERE UPPER(first_name) = $1 OR UPPER(last_name) = $1`,
+      [name.toUpperCase()]
+    );
+    return results.rows.map(c => new Customer(c));
+  }
+
+  /** get top ten customers. */
+  static async topTen() {
+    const results = await db.query(
+      `
+      SELECT c.id, first_name AS "firstName", last_name AS "lastName", phone, c.notes, customer_id, COUNT(*) 
+      FROM customers as c 
+      JOIN reservations ON c.id = customer_id
+      GROUP BY c.id, first_name, last_name, phone, c.notes, customer_id
+      ORDER BY COUNT(*) DESC
+      LIMIT 10
+      `
+    );
+    console.log(results.rows);
+    return results.rows.map(c => new Customer(c));
+  }
+
   /** get a customer by ID. */
 
   static async get(id) {
     const results = await db.query(
-          `SELECT id, 
+      `SELECT id, 
          first_name AS "firstName",  
          last_name AS "lastName", 
          phone, 
          notes 
         FROM customers WHERE id = $1`,
-        [id]
+      [id]
     );
 
     const customer = results.rows[0];
@@ -85,19 +121,20 @@ class Customer {
   async save() {
     if (this.id === undefined) {
       const result = await db.query(
-            `INSERT INTO customers (first_name, last_name, phone, notes)
+        `INSERT INTO customers (first_name, last_name, phone, notes)
              VALUES ($1, $2, $3, $4)
              RETURNING id`,
-          [this.firstName, this.lastName, this.phone, this.notes]);
+        [this.firstName, this.lastName, this.phone, this.notes]
+      );
       this.id = result.rows[0].id;
     } else {
       await db.query(
-            `UPDATE customers SET first_name=$1, last_name=$2, phone=$3, notes=$4)
+        `UPDATE customers SET first_name=$1, last_name=$2, phone=$3, notes=$4)
              WHERE id=$5`,
-          [this.firstName, this.lastName, this.phone, this.notes, this.id]);
+        [this.firstName, this.lastName, this.phone, this.notes, this.id]
+      );
     }
   }
 }
-
 
 module.exports = Customer;
--- models/reservation.js
+++ models/reservation.js
@@ -1,14 +1,13 @@
 /** Reservation for Lunchly */
 
-const moment = require("moment");
+const moment = require('moment');
 
-const db = require("../db");
-
+const db = require('../db');
 
 /** A reservation for a party */
 
 class Reservation {
-  constructor({id, customerId, numGuests, startAt, notes}) {
+  constructor({ id, customerId, numGuests, startAt, notes }) {
     this.id = id;
     this.customerId = customerId;
     this.numGuests = numGuests;
@@ -16,11 +15,23 @@ class Reservation {
     this.notes = notes;
   }
 
+  /** methods for setting/getting numGuests */
+  set numGuests(val) {
+    if (val < 1) {
+      throw new Error('Must have at least one guest.');
+    }
+    this._numGuests = val;
+  }
+
+  get numGuests() {
+    return this._numGuests;
+  }
+
   /** methods for setting/getting startAt time */
 
   set startAt(val) {
     if (val instanceof Date && !isNaN(val)) this._startAt = val;
-    else throw new Error("Not a valid startAt.");
+    else throw new Error('Not a valid startAt.');
   }
 
   get startAt() {
@@ -57,19 +68,39 @@ class Reservation {
 
   static async getReservationsForCustomer(customerId) {
     const results = await db.query(
-          `SELECT id, 
+      `SELECT id, 
            customer_id AS "customerId", 
            num_guests AS "numGuests", 
            start_at AS "startAt", 
            notes AS "notes"
          FROM reservations 
          WHERE customer_id = $1`,
-        [customerId]
+      [customerId]
     );
 
     return results.rows.map(row => new Reservation(row));
   }
-}
 
+  async save() {
+    if (this.id === undefined) {
+      const result = await db.query(
+        `INSERT INTO reservations (customer_id, num_guests, start_at, notes)
+           VALUES ($1, $2, $3, $4)
+           RETURNING id`,
+        // SELECT has an implicit return, thats why we dont need returning
+        // RETURNING is like a select of your new results
+        // In this case, we return the ID column of the new record
+        [this.customerId, this.numGuests, this.startAt, this.notes]
+      );
+      this.id = result.rows[0].id;
+    } else {
+      await db.query(
+        `UPDATE reservations SET customer_id=$1, num_guests=$2, start_at=$3, notes=$4)
+           WHERE id=$5`,
+        [this.customerId, this.numGuests, this.startAt, this.notes, this.id]
+      );
+    }
+  }
+}
 
 module.exports = Reservation;
